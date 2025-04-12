dd80e72bb70db9626dd78dfe686f3c8ea7202e01
tejprabha <dtejaa0226@gmail.com>
2025-04-13 00:41:36 +0530

    updated settings.py and added requirements.txt for deployment

--- crud_fbv/settings.py
+++ crud_fbv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mimkq=k^*__68ucbcpud28$fh8zx2e-p^jy^-ce2ou1(i72q_&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
